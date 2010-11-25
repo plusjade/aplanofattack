911bdd55843f153d768ec5f868bb59a48416e94e
plusjade <plusjade@gmail.com>
2010-11-25 15:40:33 -0800

    initial commit; aplanofattack to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aplanofattack::Application.config.secret_token = '905e2752ede4b94304a6187273d867753236fc885fdcb0fcfdd0e837673322f293ebe72ccf8f8fa3ee38f4f72eb6627bdd5ff51d3ba54f3a421c19aec50bb9da'
